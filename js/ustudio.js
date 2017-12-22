58124177d99247fb983d710fe78fe0960c25f241
Donovan Palmer <donovan.palmer@umusic.com>
2017-12-21 16:19:04 -0800

    ASF8-203: Initial javascript.

---
+++ js/ustudio.js
@@ -0,0 +1,15 @@
+/**
+ * @file
+ */
+
+(function ($, Drupal) {
+  "use strict";
+
+  Drupal.behaviors.uStudioMediaEntity = {
+    attach: function (context) {
+      function _init () {
+      }
+    }
+  };
+
+})(jQuery, Drupal);
