1703c2043b10da00e82708eb3001710992cd4ae3
Donovan Palmer <donovan.palmer@umusic.com>
2018-01-30 17:52:30 -0800

    ASF8-203: settings form working perfectly. building upload widget.

---
+++ src/Plugin/Field/FieldWidget/uStudioVideoUploadWidget.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\media_entity_ustudio\Plugin\Field\FieldWidget;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\WidgetBase;
+use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\Validator\ConstraintViolationInterface;
+
+/**
+ * Plugin implementation of the 'path' widget.
+ *
+ * @FieldWidget(
+ *   id = "path",
+ *   label = @Translation("URL alias"),
+ *   field_types = {
+ *     "path"
+ *   }
+ * )
+ */
+class uStudioVideoUploadWidget extends WidgetBase {
+
+}
